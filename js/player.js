06924d0fd70c22b1406940e9a0c2ef0b8d77dd83
unknown <Silvana Kobalenko@.globant.com>
2011-09-21 13:25:36 -0300

    Nuevos estilos por jugador

--- js/player.js
+++ js/player.js
@@ -7,14 +7,19 @@ function isFree(that)
 //Modifico la imagen dependiendo del estado de la posicion.
 function changeStateImage(that){
 	//verifico si la posicion es libre.
+	var idPlayer = $(that).attr('id');
+	
 	if (isFree(that))
 	{
 		//modifico el atributo del selector, modificando la imagen.
-		$(that).attr('src', './CSS/images/player1.png');
-		
+		$(that).attr('src', './CSS/images/' + $(that).attr('id') + '.png');
+			
+		//Modifico el estilo de la imagen.
+		$(that).addClass(idPlayer);
+			
 		//modifico la propiedad "libre" del data que corresponde al selector.
 		changeState($(that), false);
-		
+			
 		//detengo la animacion.
 		$(that).data("mouseover", false);
 	}
@@ -22,10 +27,13 @@ function changeStateImage(that){
 	{
 		//modifico el atributo del selector, modificando la imagen.
 		$(that).attr('src', $(that).data("urlImg"));
-		
+			
+		$(that).removeClass(idPlayer);
+			
 		//modifico la propiedad "libre" del data que corresponde al selector.
 		changeState($(that), true);
 	}
+	
 };
 
 //Modifico el estado del jugador. true --> libre, false --> ocupado.
@@ -33,3 +41,7 @@ function changeState(that, state)
 {
 	$(that).data("libre", state);
 }
+
+
+
+
